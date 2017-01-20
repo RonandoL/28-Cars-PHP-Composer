9c18d574f7a9ba644ac3527d9103f786c7b30467
Ronando Long <ronando.long@gmail.com>
2017-01-19 19:34:59 -0800

    all getter outputs to browser properly. Task complete, though a hatchet job

--- app/app.php
+++ app/app.php
@@ -11,11 +11,36 @@
       $lexus = new Car("Lexus RX", 85000, 75857, "http://topcarsmodels.com/wp-content/uploads/2016-Lexus-RX-350-F-Sport_01-200x200.jpg");
       $cars = array($porsche, $ford, $lexus);
 
+      echo "<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset='utf-8'>
+          <title>Ping Pong</title>
+          <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+        </head>
+
+        <body>
+          <div class='container'>
+            <h1>Car Dealership</h1>";
+
       $output = "";
       foreach ($cars as $car) {
-          $output = $output . $car->getMakeModel();
+          $output = $output . "
+              <div class='row'>
+                  <div class='col-md-3'>
+                      <img src='" .  $car->getimage() ."'>
+                  </div>
+                  <div class='col-md-3'><h3>" . $car->getMakeModel() . "</h3>
+                      <p>" . $car->getMiles() . " miles</p>
+                      <p>$" . $car->getPrice() . "</p>
+                  </div>
+
+              </div>";
       }
-      return $output;
+
+      return $output . "</div> <!-- container -->
+          </body>
+        </html>";
 
     });
 
